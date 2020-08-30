918dded4bbbdba6b85db72233e258858c32333fc
Sanford Pun <punsanford@gmail.com>
2020-08-30 21:27:15 +0800

    Assignment 3

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -19,7 +19,7 @@ promoRouter.route('/')
 	}, (err) => next(err))
 	.catch((err) => next(err))
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 	Promotions.create(req.body)
 	.then((promo) => {
 		console.log('Promotion Created ', promo);
@@ -29,12 +29,12 @@ promoRouter.route('/')
 	}, (err) => next(err))
 	.catch((err) => next(err))
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 	// Doesn't make sense
 	res.statusCode = 403;
 	res.end('PUT operation not supported on /promotions');
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 	Promotions.remove({})
 	.then((resp) => {
 		res.statusCode = 200;
@@ -54,11 +54,11 @@ promoRouter.route('/:promoId')
 	}, (err) => next(err))
 	.catch((err) => next(err))
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 	res.statusCode = 403;
 	res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   	Promotions.findByIdAndUpdate(req.params.promoId, {
 		$set: req.body
 	}, {
@@ -71,7 +71,7 @@ promoRouter.route('/:promoId')
 	}, (err) => next(err))
 	.catch((err) => next(err))
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
 	Promotions.findByIdAndRemove(req.params.promoId)
 	.then((resp) => {
 		res.statusCode = 200;
